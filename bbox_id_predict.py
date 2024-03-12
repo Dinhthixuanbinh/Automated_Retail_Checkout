64f43d624ecca4e74d44c42a17c8c1bf4fe1cefc
Quốc Long <136946649+phamvoquoclong@users.noreply.github.com>
2024-03-12 20:50:01 +0700

    Update bbox_id_predict.py

--- bbox_id_predict.py
+++ bbox_id_predict.py
@@ -11,9 +11,10 @@ def combine_tensor(tensor1, tensor2):
     return result_list
 
 
-def get_bbox(img_path):
+def get_bbox(img_path,
+             weight_path=r'best.pt'):
     # Load a model
-    model = YOLO('best.pt')  # pretrained YOLOv8n model
+    model = YOLO(weight_path)  # pretrained YOLOv8n model
     results = model(img_path)
     for result in results:
         boxes = result.boxes  # Boxes object for bounding box outputs
@@ -26,12 +27,14 @@ def parse_opt():
     parser = argparse.ArgumentParser(description='Detection Module')
     parser.add_argument("--img_path", type=str,
                         default=r'20180824-13-43-21-401.jpg')
+    parser.add_argument("--weight_path", type=str,
+                        default=r'best.pt')
     opt = parser.parse_args()
     return opt
 
 
 def main(opt):
-    get_bbox(opt.img_path)
+    get_bbox(opt.img_path, opt.weight_path)
 
 
 if __name__ == "__main__":
